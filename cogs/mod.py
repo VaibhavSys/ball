59e0a002837d3583e56b7dea0eb30a1d91b2194d
Vaibhav Dhiman <anonymouscyborg@tuta.io>
2022-01-22 17:45:23 +0530

    Added error handling for kick, ban, unban commands

--- cogs/mod.py
+++ cogs/mod.py
@@ -22,10 +22,12 @@ class Mod(commands.Cog):
 		elif isinstance(error, commands.errors.MemberNotFound):
 			await ctx.send(f"{ctx.author.mention}: I coudn't find that member.")
 		elif isinstance(error, commands.errors.CommandInvokeError):
-			await ctx.send("I do not have the required permissions to do that, either the role/member you are trying to add the role to has a role higher than the bot's highest role or the role you are trying to add is higher than the bot's highest role or I have not been granted the Manage Roles permission.")
+			await ctx.send("I do not have the required permissions to do that, either the member you are trying to add the role to has a role higher than the bot's highest role or the role you are trying to add is higher than the bot's highest role or I have not been granted the permission Manage Roles.")
 		elif isinstance(error, commands.errors.MissingRequiredArgument):
 			await ctx.send(f"Missing arguments: Please use the command like '{dfprefix}addrole <member> <role>'")
-			
+		else:
+			await ctx.send("Command Failed")
+	
 	@commands.command(pass_context=True)
 	@commands.has_permissions(manage_roles=True)
 	async def rmrole(self, ctx, member: nextcord.Member, roles: nextcord.Role):
@@ -39,21 +41,41 @@ class Mod(commands.Cog):
 		elif isinstance(error, commands.errors.MemberNotFound):
 			await ctx.send(f"{ctx.author.mention}: I coudn't find that member.")
 		elif isinstance(error, commands.errors.CommandInvokeError):
-			await ctx.send("I do not have the required permissions to do that, either the role/member you are trying to remove the role from has a role higher than the bot's highest role or the role you are trying to remove is higher than the bot's highest role or I have not been granted the Manage Roles permission.")
+			await ctx.send("I do not have the required permissions to do that, either the member you are trying to remove the role from has a role higher than the bot's highest role or the role you are trying to remove is higher than the bot's highest role or I have not been granted the permission Manage Roles.")
 		elif isinstance(error, commands.errors.MissingRequiredArgument):
 			await ctx.send(f"Missing arguments: Please use the command like '{dfprefix}rmrole <member> <role>'")
-		
+		else:
+			await ctx.send("Command Failed")
+			
 	@commands.command(pass_context=True)
 	@commands.has_permissions(kick_members=True)
 	async def kick(self, ctx, member: nextcord.Member, *, reason=None):
 		await ctx.guild.kick(user=member, reason=reason)
 		await ctx.send(f"User {member}({member.id})) has kicked by {ctx.author.mention} with reason '{reason}''.")
 	
+	@kick.error
+	async def kick_error(self, ctx, error):
+		if isinstance(error, commands.errors.MissingPermissions):
+			await ctx.send(f"{ctx.author.mention}: You do not have enough permissions (Kick Members) to use this command.")
+		elif isinstance(error, commands.errors.MemberNotFound):
+			await ctx.send(f"{ctx.author.mention}: I coudn't find that member.")
+		elif isinstance(error, commands.errors.CommandInvokeError):
+			await ctx.send("I do not have the requried permissions to do that, the member you are trying to kick has a role higher than the bot's highest role or I have not been granted the permission Kick Members.")
+	
 	@commands.command(pass_context=True)
 	@commands.has_permissions(ban_members=True)
 	async def ban(self, ctx, member : nextcord.Member, *, reason=None):
 		await member.ban(reason=reason)
 		await ctx.send(f"{member} has been banned by {ctx.author.mention} with reason '{reason}'.")
+		
+	@ban.error
+	async def ban_error(self, ctx, error):
+		if isinstance(error, commands.errors.MissingPermissions):
+			await ctx.send(f"{ctx.author.mention}: You do not have enough permissions (Ban Members) to use this command.")
+		elif isinstance(error, commands.errors.MemberNotFound):
+			await ctx.send(f"{ctx.author.mention}: I coudn't find that member.")
+		elif isinstance(error, commands.errors.CommandInvokeError):
+			await ctx.send("I do not have the requried permissions to do that, the member you are trying to ban has a role higher than the bot's highest role or I have not been granted the permission Ban Members.")
 		
 	@commands.command(pass_context=True)
 	@commands.has_permissions(ban_members=True)
@@ -68,6 +90,16 @@ class Mod(commands.Cog):
 				await ctx.guild.unban(user, reason=reason)
 				await ctx.send(f"{user.name}#{user.discriminator} has been unbanned by {ctx.author.mention} with reason '{reason}'")
 
-	
+	@unban.error
+	async def unban_error(self, ctx, error):
+		if isinstance(error, commands.errors.MissingPermissions):
+			await ctx.send(f"{ctx.author.mention}: You do not have enough permissions (Ban Members) to use this command.")
+		elif isinstance(error, commands.errors.MemberNotFound):
+			await ctx.send(f"{ctx.author.mention}: I coudn't find that member.")
+		elif isinstance(error, commands.errors.CommandInvokeError):
+			await ctx.send("I do not have the requried permissions (Ban Members) to do that.")
+		else:
+			await ctx.send("Unban Failed")
+			
 def setup(bot):
 	bot.add_cog(Mod(bot))
