01fe4ef2d25bdbd550c1155263f2516dda776f7a
Vaibhav Dhiman <anonymouscyborg@tuta.io>
2022-02-22 22:07:38 +0530

    modified:   .gitignore 	deleted:    foo.py 	modified:   pyproject.toml

--- foo.py
+++
@@ -1,5 +0,0 @@
-from os import system
-depend = ['nextcord', 'nextcord.ext', 'nextcord.utils', 'flask', 'threading', 'os', 'time', 'json', 'logging', 'dotenv', 'sys']
-
-for dep in depend:
-    system(f'python3 -m poetry add {dep}')
