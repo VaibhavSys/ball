0b0db89a0715b694738eaabbc5f9ffc45594edaa
Vaibhav Dhiman <anonymouscyborg@tuta.io>
2022-03-08 23:37:53 +0530

    Removed unnececary imports and removed trailing whitespaces

--- src/bot.py
+++ src/bot.py
@@ -1,50 +1,53 @@
-import requests
 import nextcord
 import os
-import time
 import nextcord.ext
-from nextcord.utils import get
-from nextcord.ext import commands, tasks
-from nextcord.ext.commands import has_permissions,  CheckFailure, check
-import json
+from nextcord.ext import commands
 import logging
-from sys import argv
 import keep_alive
+from os import getenv
 
 intents = nextcord.Intents.default()
 intents.members = True
-TOKEN = os.getenv('TOKEN')
-bot = commands.Bot(command_prefix = "-", intents=intents)
+bot = commands.Bot(command_prefix="-", intents=intents)
 cogs = []
 
-#Logging Setup
+
+# Logging Setup
 logger = logging.getLogger('nextcord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='nextcord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='nextcord.log', encoding='utf-8', mode='w'
+    )
+
+handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    )
 logger.addHandler(handler)
 
-# Events
 
+# Events
 @bot.event
 async def on_connect():
     print("----------\nConnected successfully.\n----------")
+
 
 @bot.event
 async def on_ready():
     print(f"----------\nLogged in as {bot.user}({bot.user.id}).\n----------")
 
+
 @bot.event
 async def on_disconnect():
     print("Disconnected.")
 
-for file in os.listdir("./cogs"): 
-    if file.endswith(".py"): 
-        name = file[:-3] 
-        cogs.append(f"cogs.{name}")
-        bot.load_extension(f"cogs.{name}")
+for file in os.listdir("./cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        cog = f"cogs.{name}"
+        cogs.append(cog)
+        bot.load_extension(cog)
         print(f"Loaded cog {name}.")
-        
-keep_alive.keep_alive()
-bot.run(TOKEN) 
 
+
+keep_alive.keep_alive()
+bot.run(getenv("TOKEN"))
