c27ae8422524886afab29da4717b5c73bd91bf96
Vaibhav Dhiman <mousemoosz@protonmail.com>
2022-05-21 22:19:32 +0530

    Grouped imports in mod_vc.py and remove unused var

--- src/cogs/mod_vc.py
+++ src/cogs/mod_vc.py
@@ -1,8 +1,7 @@
 import nextcord
-from nextcord.ext import commands, application_checks
 from nextcord import SlashOption
+from nextcord.ext import commands, application_checks
 
-TESTING_GUILD_ID = 923519688871411732
 
 class ModVC(commands.Cog):
     """
